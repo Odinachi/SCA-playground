1fa5d3694ecbb39649119fa39fbc4b3fe38a3cef
Odinachi David <odinachidavid@yahoo.com>
2024-12-04 13:41:04 +0100

    added assets

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color appBlue = Color(0xff4461F2);
+  static const Color lightGray = Color(0xffC7C7C7);
+  static const Color gray = Color(0xff667085);
+}
