0378206b641b17d6bf7b3d4ea1666bfcb88edb1d
Odinachi David <odinachidavid@yahoo.com>
2024-12-12 14:45:27 +0100

    alerts

--- lib/features/back_values/pop_value_screen.dart
+++ lib/features/back_values/pop_value_screen.dart
@@ -16,11 +16,13 @@ class _PopValueScreenState extends State<PopValueScreen> {
     _controller.dispose();
   }
 
+  double? value;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Return pop value"),
+        title: const Text("Return pop value"),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
@@ -35,15 +37,96 @@ class _PopValueScreenState extends State<PopValueScreen> {
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.grey.shade100,
-                border: OutlineInputBorder(),
+                border: const OutlineInputBorder(),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.pop(context, _controller.text);
+                  // showModalBottomSheet(
+                  //     backgroundColor: Colors.blue.withOpacity(.2),
+                  //     isScrollControlled: true,
+                  //     context: context,
+                  //     builder: (_) => Column(
+                  //           mainAxisAlignment: MainAxisAlignment.center,
+                  //           children: [
+                  //             CupertinoActionSheet(
+                  //               message: Text("Your was login successful"),
+                  //               title: Text("Login successful"),
+                  //               actions: [
+                  //                 CupertinoActionSheetAction(
+                  //                     onPressed: () {},
+                  //                     child: Text("Thank you")),
+                  //                 CupertinoActionSheetAction(
+                  //                     onPressed: () {
+                  //                       Navigator.pop(context);
+                  //                     },
+                  //                     child: Text("Let me in")),
+                  //               ],
+                  //             ),
+                  //           ],
+                  //         ));
+
+                  // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  //   elevation: 0,
+                  //   content: Text(
+                  //     "Hello",
+                  //     style: TextStyle(
+                  //       fontWeight: FontWeight.w600,
+                  //     ),
+                  //   ),
+                  //   behavior: SnackBarBehavior.floating,
+                  //   action: SnackBarAction(
+                  //       label: "Close",
+                  //       onPressed: () {
+                  //         ScaffoldMessenger.of(context).clearSnackBars();
+                  //       }),
+                  //   backgroundColor: Colors.red,
+                  // ));
+
+                  //
+                  // showDialog(
+                  //   context: context,
+                  //   builder: (_) => CupertinoActionSheet(
+                  //     message: Text("Your was login successful"),
+                  //     title: Text("Login successful"),
+                  //     actions: [
+                  //       CupertinoActionSheetAction(
+                  //           onPressed: () {}, child: Text("Thank you")),
+                  //       CupertinoActionSheetAction(
+                  //           onPressed: () {
+                  //             Navigator.pop(context);
+                  //           },
+                  //           child: Text("Let me in")),
+                  //     ],
+                  //   ),
+                  // );
+                  //
+                  //
+                  // showDialog(
+                  //     barrierDismissible: false,
+                  //     context: context,
+                  //     builder: (_) => AlertDialog(
+                  //           backgroundColor: Colors.green.shade200,
+                  //           content: Text(
+                  //             "Login successful",
+                  //             style: TextStyle(
+                  //                 fontSize: 30, fontWeight: FontWeight.w600),
+                  //           ),
+                  //           actions: [
+                  //             ElevatedButton(
+                  //                 onPressed: () {}, child: Text("Thank you")),
+                  //             ElevatedButton(
+                  //                 onPressed: () {
+                  //                   Navigator.pop(context);
+                  //                 },
+                  //                 child: Text("Let me in")),
+                  //           ],
+                  //         ));
+
+                  // Navigator.pop(context, _controller.text);
                 },
                 child: Text("Return value"))
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:secondapp/features/Navigation/router.dart';
 import 'package:secondapp/features/navigation/route_strings.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       onGenerateRoute: AppRouter.appRouter,
-      initialRoute: AppRouteStrings.loginScreen,
+      initialRoute: AppRouteStrings.base,
     );
   }
 }
